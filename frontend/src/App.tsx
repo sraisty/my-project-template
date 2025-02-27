07953d56b0a5cfcac25ad37b4249bfc5332eba97
Sue Raisty <sraisty@gmail.com>
2025-02-27 05:40:41 -0800

    tailwind installed.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,21 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { ProgressBarController } from './components/ProgressBarController'
 
 export const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
+      <div className="flex flex-row">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <h2>My Progress Bar</h2>
+      <h1 className="text-3xl text-blue-500">Vite + React</h1>
+      <h2 className="text-1xl text-red-300">My Progress Bar</h2>
       <ProgressBarController />
     </>
   )
 }
-
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,10 @@ import { defineConfig } from 'vitest/config'
 // import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { configDefaults } from 'vitest/config'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
