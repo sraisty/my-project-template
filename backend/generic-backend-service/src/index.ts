d65c00fd51a3945a13354a17bd200d3114092c50
Sue Raisty <sraisty@gmail.com>
2025-02-28 23:13:07 -0800

    Move package dependencies into a hierarchy, mono-repo style

--- backend/generic-backend-service/src/index.ts
+++ backend/generic-backend-service/src/index.ts
@@ -17,5 +17,5 @@ app.get('/', (_req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
+  console.log(`🚀 generic-backend-service is running on http://localhost:${PORT}`)
 })
